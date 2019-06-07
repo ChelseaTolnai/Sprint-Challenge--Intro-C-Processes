e7c7300166ccb326c678728d7486bd77db5ca0ea
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-06-07 12:20:31 -0400

    Changes to parse command and work on open directory.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -9,13 +10,18 @@ int main(int argc, char **argv)
   // Parse command line
   if (argc > 2) {
     fprintf(stderr, "You cannot provide more than two arguments.\n");
-    return -1;
+    exit(1);
   } 
-  char *directory = (argc == 2) ? argv[1] : argv[0];
+  const char *path = (argc == 2) ? argv[1] : ".";
 
-  printf("%s\n", directory);
+  printf("%s\n", path);
 
   // Open directory
+  DIR *opendir(const char *path);
+  if (&opendir == NULL) {
+    fprintf(stderr, "Error opening directory.\n");
+    exit(1);
+  } 
 
   // Repeatly read and print entries
 
