1d4112590a432f6691c0b152f5cb4454142e13d6
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-06-07 14:44:53 -0400

    Stretch: Expand the Hierarchy

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -31,7 +32,28 @@ int main(int argc, char **argv)
     stat(file_name, &buf);
     
     if ((buf.st_mode & S_IFDIR) != 0) {
+      char *path_sub = (char *) malloc(strlen(path) + 1 + strlen(file_name) + 1);
+      strcpy(path_sub, path);
+      strcat(path_sub, "/");
+      strcat(path_sub, file_name);
+
+      DIR *d_sub = opendir(path_sub);
+      if (d_sub == NULL) {
+        printf("%10lld  %s\n", buf.st_size, file_name);
+        continue;
+      } 
       printf("%10s  %s\n", "<DIR>", file_name);
+      struct dirent *ent_sub;
+      struct stat buf_sub;
+      while ((ent_sub = readdir(d_sub)) != NULL) {
+        char *file_name_sub = ent_sub->d_name;
+        stat(file_name_sub, &buf_sub);
+        if ((buf_sub.st_mode & S_IFDIR) != 0) {
+          printf("          %10s  %s\n", "<DIR>", file_name_sub);
+        } else {
+          printf("          %10lld  %s\n", buf.st_size, file_name_sub);
+        }
+      }
     } else {
       printf("%10lld  %s\n", buf.st_size, file_name);
     }
